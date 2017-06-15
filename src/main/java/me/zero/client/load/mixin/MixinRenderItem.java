4ccd7ba9321d95ec7236f6ad4d932998a9f4455e
LeafHacker <leaf@leafhacker.co.uk>
2017-06-15 09:05:38 +0100

    Update to 1.12 mappings

--- src/main/java/me/zero/client/load/mixin/MixinRenderItem.java
+++ src/main/java/me/zero/client/load/mixin/MixinRenderItem.java
@@ -32,7 +32,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(RenderItem.class)
 public class MixinRenderItem {
 
-    @Inject(method = "func_191966_a", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "renderEffect", at = @At("HEAD"), cancellable = true)
     public void renderEffect(IBakedModel model, CallbackInfo ci) {
         GlintEffectEvent event = new GlintEffectEvent(GlintEffectEvent.GlintTarget.ITEM);
         ClientAPI.EVENT_BUS.post(event);
